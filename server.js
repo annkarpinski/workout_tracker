2a24aeaa13078d1e6f17685a82d1fe52f1817e5a
Ann Karpinski <annkarpinski@gmail.com>
2020-07-26 17:34:18 -0500

    updated routes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +18,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/workouts-api.js"));
+app.use(require("./routes/view-html.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
+
