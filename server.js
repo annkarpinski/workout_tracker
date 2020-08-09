00bf32a1cefe4f9d154f532c7839613aafbfc289
Ann Karpinski <annkarpinski@gmail.com>
2020-08-09 18:26:01 -0500

    fixing mLab Heroku connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
